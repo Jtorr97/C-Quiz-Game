8bfe2381fed97b4210ecd226747f4bd2f81f4831
Jtorr97 <torresj1997@outlook.com>
2016-12-11 02:45:08 -0600

    Cleanup
    Final Product I turned in for class.

--- semesterprogram/c-quiz.cpp
+++ semesterprogram/c-quiz.cpp
@@ -1,3 +1,8 @@
+//--------------------------------------------------------
+//
+//c-quiz.cpp
+//
+//--------------------------------------------------------
 #include <iostream>
 #include <fstream>
 #include <string>
@@ -7,14 +12,7 @@
 #include <random>
 
 namespace {
-	const int s_questionScore = 10;  // Points rewarded for each correct answer.
-	const int s_failingGrade = 50;	
-	const int s_numQuestions = 10; 
-	const char* s_winMessage = "Correct!\n";
-	const char* s_loseMessage = "Incorrect, the correct answer was ";
-	const char* s_quizFailed = "Sorry, you failed... Better luck next time.";
-	const char* s_promptAnswer = "What is your answer?\n";
-	static int totalScore = 0;
+	int s_totalScore = 0;
 }
 
 class Question {
@@ -23,7 +21,7 @@ public:
 	void PrintResults();
 	bool Run(std::ifstream& fin);
 	bool InitializeQuizGame(std::ifstream& fin);
-	int ask();
+	int askQuestion(int num = -1);
 	friend std::istream& operator >> (std::istream& is, Question& ques);
 
 
@@ -37,48 +35,25 @@ private:
 	char correct_answer;
 };
 
+void PrintTxtMsg(std::ifstream txtfile);
 void LoadData(std::istream& is, std::vector<Question>& questions);
 void Shuffle(std::vector<Question>& questions);
+bool AskToPlayAgain();
 
 int main()
 {
-	Question Game;
-
-	Game.Run(std::ifstream("quiz_data.txt"));
-
-	system("pause");
+	int s_totalScore = 0;
+	Question Quiz;
+	
+	bool playAgain = false;
+	do {
+		Quiz.Run(std::ifstream("quiz_data.txt"));
+		playAgain = AskToPlayAgain();
+	} while (playAgain);
 
 	return 0;
 }
 
-void Question::AskTheQuestions()
-{
-	for (int i = 0; i < s_numQuestions; ++i)
-	{
-		questions[i].ask();
-	}
-}
-
-void Question::PrintResults()
-{
-	std::cout << "Your total score is " << totalScore << " out of 100!";
-	
-	
-}
-
-bool Question::Run(std::ifstream& quiz_data)
-{
-	if (InitializeQuizGame(quiz_data)) {
-		AskTheQuestions();
-		PrintResults();
-		return true;
-	}
-	else
-	{
-		return false;
-	}
-
-}
 bool Question::InitializeQuizGame(std::ifstream& quiz_data)
 {
 	if (quiz_data.is_open())
@@ -93,6 +68,58 @@ bool Question::InitializeQuizGame(std::ifstream& quiz_data)
 		return false;
 	}
 	std::cin.get();
+}
+
+void LoadData(std::istream& is, std::vector<Question>& questions)
+{
+	Question q;
+	while (is >> q)
+		questions.push_back(q);
+}
+
+void Shuffle(std::vector<Question>& questions)
+{
+	std::random_device rd;
+	std::mt19937 randomGenerator(rd());
+	std::shuffle(questions.begin(), questions.end(), randomGenerator);
+}
+
+bool Question::Run(std::ifstream& quiz_data)
+{
+	if (InitializeQuizGame(quiz_data)) {
+		PrintTxtMsg(std::ifstream("welcome.txt"));
+		AskTheQuestions();
+		PrintResults();
+		return true;
+	}
+	else
+	{
+		return false;
+	}
+
+}
+
+void Question::AskTheQuestions()
+{
+	const int s_numQuestions = 10;
+	for (int i = 0; i < s_numQuestions; ++i)
+	{
+		questions[i].askQuestion(i + 1);
+	}
+}
+
+void Question::PrintResults()
+{
+	const int s_failingGrade = 50;
+	if (s_totalScore >= s_failingGrade) {
+		std::cout << "Your total score is " << s_totalScore << " out of 100!\n";
+		PrintTxtMsg(std::ifstream("quiz_passed.txt"));
+	}
+	else
+	{
+		std::cout << "Your total score is " << s_totalScore << " out of 100...\n";
+		std::cout << "Sorry, you failed the quiz. Better luck next time.\n";
+	}
 }
 
 std::istream& operator >> (std::istream& is, Question& ques)
@@ -113,54 +140,65 @@ std::istream& operator >> (std::istream& is, Question& ques)
 	return is;
 }
 
-void LoadData(std::istream& is, std::vector<Question>& questions)
-{
-	Question q;
-	while (is >> q)
-		questions.push_back(q);
-}
-
 int TrackScore()
 {
-	totalScore = totalScore + s_questionScore;
-
-	return totalScore;
+	const int s_questionScore = 10;  // Points rewarded for each correct answer.
+	s_totalScore = s_totalScore + s_questionScore;
+	return s_totalScore;
 }
 
-int Question::ask()
+int Question::askQuestion(int num)
 {
-	int score = 0;
-
 	std::cout << "\n";
-	std::cout << question_text << "\n";
+	if (num > 0)
+		std::cout << num << ".) ";
+	std::cout << question_text << "\n\n";
 	std::cout << "a. " << answer_1 << "\n";
 	std::cout << "b. " << answer_2 << "\n";
 	std::cout << "c. " << answer_3 << "\n";
-	std::cout << "d. " << answer_4 << "\n";
+	std::cout << "d. " << answer_4 << "\n\n";
 
 	//Ask user for their answer.
 	char guess = ' ';
-	std::cout << s_promptAnswer;
+	std::cout << "What is your answer?\n";
 	std::cin >> guess;
 
 	if (guess == correct_answer) {
-		std::cout << s_winMessage;
+		std::cout << "Correct!\n";
 		TrackScore();
 		std::cin.get();
 		std::cin.get();
 	}
 	else
 	{
-		std::cout << s_loseMessage << correct_answer << ".\n";
+		std::cout << "Incorrect, the correct answer was " << correct_answer << ".\n";
 		std::cin.get();
 		std::cin.get();
 	}
-	return score;
+	return num;
 }
 
-void Shuffle(std::vector<Question>& questions)
+bool AskToPlayAgain()
 {
-	std::random_device rd;
-	std::mt19937 randomGenerator(rd());
-	std::shuffle(questions.begin(), questions.end(), randomGenerator);
+	std::cout << "Do you wanna play again? ";
+	std::string Response = " ";
+	std::getline(std::cin, Response);
+
+	return (Response[0] == 'y') || (Response[0] == 'Y');
 }
+
+void PrintTxtMsg(std::ifstream txt_art)
+{
+	std::string line;
+	std::ifstream txtfile();
+	if (txt_art.is_open())
+	{
+		while (getline(txt_art, line))
+		{
+			std::cout << line << '\n';
+		}
+		txt_art.close();
+	}
+	else std::cout << "Unable to open file. :(";
+}
+
